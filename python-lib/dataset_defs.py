af4ecb921b2177cef96dcb140f1949f436330316
wnowak <wnowak>
2019-01-29 19:48:53 +0100

    Edited file 'python-lib/dataset_defs.py'

--- python-lib/dataset_defs.py
+++ python-lib/dataset_defs.py
@@ -86,7 +86,8 @@ params = {
           u'includeRules': [],
           u'mode': u'ALL'},
          u'notReadyIfEmpty': False,
-         u'path': u'${projectKey}/filesystem_managed_csv'},
+         u'path': u'${projectKey}/filesystem_managed_csv'
+    },
 	"sql" : {
 	  'connection': 'postgresql-10',
 	  'mode': 'table',
