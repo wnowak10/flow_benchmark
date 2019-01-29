00d95d0da5d7716b9cc83ef81262fba8ba01b233
wnowak <wnowak>
2019-01-29 21:55:33 +0100

    Edited file 'python-lib/benchmark.py'

--- python-lib/benchmark.py
+++ python-lib/benchmark.py
@@ -133,11 +133,11 @@ class checkpoint_flow(object):
         #       * formatType
         #       * type
         #       * params
-        print(connection_type)
-        print(new_format)
-        print(dataset_defs.formatParams.keys())
-        print(dataset_defs.formatParams['file_system_managed'].keys())
-        print(dataset_defs.formatParams[connection_type])
+#         print(connection_type)
+#         print(new_format)
+#         print(dataset_defs.formatParams.keys())
+#         print(dataset_defs.formatParams['file_system_managed'].keys())
+#         print(dataset_defs.formatParams[connection_type])
 
         formatParams = dataset_defs.formatParams[connection_type][new_format]
         changed['formatParams'] = formatParams
