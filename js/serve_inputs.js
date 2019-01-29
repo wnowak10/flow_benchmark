9f69815f7723e42228fc48eeab80f66d60c87687
wnowak <wnowak>
2019-01-29 23:47:21 +0100

    Edited file 'js/serve_inputs.js'

--- js/serve_inputs.js
+++ js/serve_inputs.js
@@ -2,7 +2,7 @@ var app = angular.module('update_instance_info.module', []);
 
 app.controller('FoobarController', function($scope) {
 
-    var updateProjects = function() {
+    var updateDatasets = function() {
         // the parameter to callPythonDo() is passed to the do() method as the payload
         // the return value of the do() method comes back as the data parameter of the fist function()
         $scope.callPythonDo({"datasets": "datasets"}).then(function(data) {
