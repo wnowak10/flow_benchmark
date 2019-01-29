2374fb89e0c956f36ed0afa6475ea9796d5bd27b
wnowak <wnowak>
2019-01-29 23:33:59 +0100

    Edited file 'resource/get_instance_info.py'

---
+++ resource/get_instance_info.py
@@ -0,0 +1,20 @@
+import dataiku
+
+def do(payload, config, plugin_config, inputs):
+    
+    client = dataiku.api_client()
+    
+    if payload["funtastic"] == "projects":
+        project_list = client.list_project_keys()
+    
+        return{'projects' : project_list}
+    
+    if payload["funtastic"] == "folders":
+        current_proj_key = dataiku.get_custom_variables()['projectKey']
+        proj = client.get_project(current_proj_key)
+        folder_names = []
+        folders = proj.list_managed_folders()
+        for folder in folders:
+            folder_names.append(folder['name'])
+            
+        return{'folders' : folder_names}
