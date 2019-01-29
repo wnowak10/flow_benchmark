adabb2a6464a8645b91511d2aaeae530303721b5
wnowak <wnowak>
2019-01-29 23:34:48 +0100

    Edited file 'js/serve_inputs.js'

---
+++ js/serve_inputs.js
@@ -0,0 +1,34 @@
+var app = angular.module('update_instance_info.module', []);
+
+app.controller('FoobarController', function($scope) {
+
+    var updateProjects = function() {
+        // the parameter to callPythonDo() is passed to the do() method as the payload
+        // the return value of the do() method comes back as the data parameter of the fist function()
+        $scope.callPythonDo({"funtastic": "projects"}).then(function(data) {
+            // success
+            $scope.projects = data.projects;
+        }, function(data) {
+            // failure
+            $scope.projects = [];
+        });
+    };
+    updateProjects();
+    $scope.$watch('config.allProjects', updateProjects);
+    
+    var updateFolders = function() {
+        // the parameter to callPythonDo() is passed to the do() method as the payload
+        // the return value of the do() method comes back as the data parameter of the fist function()
+        $scope.callPythonDo({"funtastic": "folders"}).then(function(data) {
+            // success
+            $scope.folders = data.folders;
+        }, function(data) {
+            // failure
+            $scope.folders = [];
+        });
+    };
+    updateFolders();
+    $scope.$watch('config.logFolder', updateFolders);
+});
+
+
