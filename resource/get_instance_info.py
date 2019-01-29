d1ea11fa17d718a2a9fe4def2b48d62b95c0b236
wnowak <wnowak>
2019-01-29 23:43:43 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -7,7 +7,7 @@ def do(payload, config, plugin_config, inputs):
     project = client.get_project(pkey)
     
     if payload["funtastic"] == "datasets":
-        project_list = client.list_project_keys()
+        project_list = project.list_datasets()
     
         return{'projects' : project_list}
     
