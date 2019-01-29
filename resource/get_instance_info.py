a733d9d5256f6690d2d5e5e5c61d7f3b01f9379b
wnowak <wnowak>
2019-01-29 23:40:49 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -3,6 +3,7 @@ import dataiku
 def do(payload, config, plugin_config, inputs):
     
     client = dataiku.api_client()
+    client.get_project("NOWAK_SANDBOX")
     
     if payload["funtastic"] == "projects":
         project_list = client.list_project_keys()
