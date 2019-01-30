69e768c2b2300133699aeeb727d011f283cfd41b
wnowak <wnowak>
2019-01-30 17:21:27 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -5,24 +5,25 @@ from sets import Set
 # config and plugin_config are the recipe/dataset and plugin configured values
 # inputs is the list of input roles (in case of a recipe)
 def do(payload, config, plugin_config, inputs):
-    role_name = 'input_role'
-    # get dataset name then dataset handle
-    dataset_full_names = [i['fullName'] for i in inputs if i['role'] == role_name]
-    if len(dataset_full_names) == 0:
-        return {'choices' : []}
-    dataset = Dataset(dataset_full_names[0])
-    # get name of column providing the choices
-    column_name = config.get('filterColumn', '')
-    if len(column_name) == 0:
-        return {'choices' : [inputs]}
-    # check that the column is in the schema
-    schema = dataset.read_schema()
-    schema_columns = [col for col in schema if col['name'] == column_name]
-    if len(schema_columns) != 1:
-        return {'choices' : [inputs]}
-    schema_column = schema_columns[0]
-    # get the data and build the set of values
-    choices = Set()
-    for row in dataset.iter_tuples(sampling='head', limit=10000, columns=[column_name]):
-        choices.add(row[0])
-    return {'choices' : list(choices)}
+    return {'choices': ['a','will']}
+#     role_name = 'input_role'
+#     # get dataset name then dataset handle
+#     dataset_full_names = [i['fullName'] for i in inputs if i['role'] == role_name]
+#     if len(dataset_full_names) == 0:
+#         return {'choices' : []}
+#     dataset = Dataset(dataset_full_names[0])
+#     # get name of column providing the choices
+#     column_name = config.get('filterColumn', '')
+#     if len(column_name) == 0:
+#         return {'choices' : []}
+#     # check that the column is in the schema
+#     schema = dataset.read_schema()
+#     schema_columns = [col for col in schema if col['name'] == column_name]
+#     if len(schema_columns) != 1:
+#         return {'choices' : []}
+#     schema_column = schema_columns[0]
+#     # get the data and build the set of values
+#     choices = Set()
+#     for row in dataset.iter_tuples(sampling='head', limit=10000, columns=[column_name]):
+#         choices.add(row[0])
+#     return {'choices' : list(choices)}
