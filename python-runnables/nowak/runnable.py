793823776c2ffe68b3bd69cc6cf1ae5ccf52057d
wnowak <wnowak>
2019-01-30 22:26:18 +0100

    Edited file 'python-runnables/nowak/runnable.py'

--- python-runnables/nowak/runnable.py
+++ python-runnables/nowak/runnable.py
@@ -67,8 +67,7 @@ class MyRunnable(Runnable):
         cf.reformat_flow(formatType, computeEngine)
         
 #         cf.reformat_flow(self.config['formatType'], self.config['filterValue'])  # Works.
-        print("XXXXXXXXXX \n\n\n\n\n")
-        print(self.config)
+
         cf.set_compute_engines(computeEngine)
 #         cf.set_compute_engines(self.config['computeEngine']) # Works
         flow_results = cf.build_flow()
