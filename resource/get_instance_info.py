e2738d64c43e0c2acf4d3a651aee381f64e3f66a
wnowak <wnowak>
2019-01-30 20:05:28 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -11,7 +11,7 @@ def do(payload, config, plugin_config, inputs):
     engines = project.get_settings().get_raw()['metrics']['engineConfig'].keys()
 #     projects = [i['name'] for i in client.list_projects()]
     connections = client.list_connections().keys()
-    return {'choices': engines}
+    return {'choices': connections}
 #     role_name = 'input_role'
 #     # get dataset name then dataset handle
 #     dataset_full_names = [i['fullName'] for i in inputs if i['role'] == role_name]
