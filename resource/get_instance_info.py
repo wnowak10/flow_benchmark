82a6370775300d6aad1eb560ec689b891606c3a7
wnowak <wnowak>
2019-01-30 17:22:26 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -6,7 +6,7 @@ from sets import Set
 # config and plugin_config are the recipe/dataset and plugin configured values
 # inputs is the list of input roles (in case of a recipe)
 def do(payload, config, plugin_config, inputs):
-    return {'choices': ['a','will']}
+    return {'choices': dataiku.list_datasets()}
 #     role_name = 'input_role'
 #     # get dataset name then dataset handle
 #     dataset_full_names = [i['fullName'] for i in inputs if i['role'] == role_name]
