e9c87182a699e1f1631f4f943e7b7fe7e63d0b68
wnowak <wnowak>
2019-01-30 22:14:48 +0100

    Edited file 'python-lib/benchmark.py'

--- python-lib/benchmark.py
+++ python-lib/benchmark.py
@@ -277,6 +277,8 @@ class checkpoint_flow(object):
         Spark pipelines when we build datasets one by one
         as we do in `build_flow`?
         """
+        print("XXXXXXX \n\n\n\n")
+        print(able)
         s = self.client.get_project(self.project_key).get_settings()
         r = s.get_raw()
         r['settings']['flowBuildSettings']['mergeSparkPipelines'] = able
