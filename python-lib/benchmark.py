1dfc103c99fb09c925df91eb3e1d4291baf658dd
wnowak <wnowak>
2019-01-30 19:19:09 +0100

    Edited file 'python-lib/benchmark.py'

--- python-lib/benchmark.py
+++ python-lib/benchmark.py
@@ -149,7 +149,8 @@ class checkpoint_flow(object):
             
             
         changed['params'] = dataset_defs.params[connection_type]
-
+        connection_type == "file_system_managed":
+            changed['params']['path'] = '${projectKey}/'+dataset_name
         if connection_type == "file_system_managed":
             changed['type']= 'Filesystem'
         elif connection_type == 'hdfs': 
