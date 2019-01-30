0e3c29e919075c0dc109addd1af47071e53b32f0
wnowak <wnowak>
2019-01-30 22:05:47 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -2,9 +2,6 @@ from dataiku import Dataset
 import dataiku
 from sets import Set
 
-# Payload is sent from the javascript's callPythonDo()
-# config and plugin_config are the recipe/dataset and plugin configured values
-# inputs is the list of input roles (in case of a recipe)
 def do(payload, config, plugin_config, inputs):
     client      = dataiku.api_client()
     project = client.get_project(dataiku.default_project_key())
