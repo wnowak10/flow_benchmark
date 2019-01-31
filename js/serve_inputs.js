5c19d3ac5197a9745cdd6be8a1cc67e0b704a175
wnowak <wnowak>
2019-01-31 14:11:52 +0100

    Edited file 'js/serve_inputs.js'

--- js/serve_inputs.js
+++ js/serve_inputs.js
@@ -4,7 +4,7 @@ app.controller('FoobarController', function($scope) {
     var updateChoices = function() {
         // the parameter to callPythonDo() is passed to the do() method as the payload
         // the return value of the do() method comes back as the data parameter of the fist function()
-        $scope.callPythonDo({}).then(function(data) {
+        $scope.callPythonDo({"funtastic": "engines"}).then(function(data) {
             // success
             console.log(data);
             $scope.choices = data.choices;
