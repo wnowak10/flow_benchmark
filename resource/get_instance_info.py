b362086c005c749b9f16ee72a02152f338ff2988
wnowak <wnowak>
2019-01-31 17:16:57 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -1,10 +1,10 @@
-from dataiku import Dataset
+# from dataiku import Dataset
 import dataiku
-from sets import Set
+# from sets import Set
 
 def do(payload, config, plugin_config, inputs):
     if payload["funtastic"] == "engines":
-        client      = dataiku.api_client()
+        client  = dataiku.api_client()
         project = client.get_project(dataiku.default_project_key())
         engines = project.get_settings().get_raw()['metrics']['engineConfig'].keys()     
         return {'engines': engines}
