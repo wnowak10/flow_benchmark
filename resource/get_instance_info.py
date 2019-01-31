0184e11da1ea615d9d5d340705d991db6e08eb39
wnowak <wnowak>
2019-01-31 17:16:33 +0100

    Edited file 'resource/get_instance_info.py'

--- resource/get_instance_info.py
+++ resource/get_instance_info.py
@@ -7,13 +7,11 @@ def do(payload, config, plugin_config, inputs):
         client      = dataiku.api_client()
         project = client.get_project(dataiku.default_project_key())
         engines = project.get_settings().get_raw()['metrics']['engineConfig'].keys()
-
-    #     connections = client.list_connections().keys()
+        
         return {'engines': engines}
     
     
     if payload["funtastic"] == "connections":
         client      = dataiku.api_client()
-
         connections = client.list_connections().keys()
         return {'connections': connections}
