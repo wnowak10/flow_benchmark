b1245a3fc62a7e244bcc63102c7ce18c90e8481b
wnowak <wnowak>
2019-01-31 14:14:09 +0100

    Edited file 'js/serve_inputs.js'

--- js/serve_inputs.js
+++ js/serve_inputs.js
@@ -16,4 +16,21 @@ app.controller('FoobarController', function($scope) {
     };
     updateChoices();
     $scope.$watch('config.filterColumn', updateChoices);
+    
+    var updateConnections = function() {
+        // the parameter to callPythonDo() is passed to the do() method as the payload
+        // the return value of the do() method comes back as the data parameter of the fist function()
+        $scope.callPythonDo({"funtastic": "engines"}).then(function(data) {
+            // success
+            console.log(data);
+            $scope.choices = data.choices;
+        }, function(data) {
+            // failure
+            console.log(data);
+            $scope.choices = [];
+        });
+    };
+    updateChoices();
+    $scope.$watch('config.filterColumn', updateConnections);
+// });
 });
