5c31335eca181cf5df4aba791a70d97416ac1ca2
wnowak <wnowak>
2019-02-07 20:11:13 +0100

    Edited file 'python-runnables/nowak/runnable.py'

--- python-runnables/nowak/runnable.py
+++ python-runnables/nowak/runnable.py
@@ -98,8 +98,10 @@ class MyRunnable(Runnable):
         
         res += html_template.table
         
+        total_build_time_sum = 0
         for i, _ in enumerate(flow_results):    
             res += html_row([flow_results.keys()[i], flow_results.values()[i]])
+            total_build_time_sum += flow_results.values()[i]
         res+="""
         </table>
         </body>
