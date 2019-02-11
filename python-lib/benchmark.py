54d2278df840e0d541f7f686d33edff0dc01d9d0
wnowak <wnowak>
2019-02-11 22:40:23 +0100

    Edited file 'python-lib/benchmark.py'

--- python-lib/benchmark.py
+++ python-lib/benchmark.py
@@ -162,7 +162,7 @@ class checkpoint_flow(object):
             changed['params']['bucket'] = s3Bucket
             changed['params']['path'] = '/dataiku/${projectKey}/' + dataset_name
         if connectionType =='Azure':
-            pass  # What to do for Azure connection?
+            changed['params']['path'] = '/dataiku/${projectKey}/' + dataset_name  # What to do for Azure connection?
         
         """
         ### TO DO!!! ###
